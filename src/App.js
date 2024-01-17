3a0e26dd5cc9dca24b0678e21d2876945d256e77
FernandoPR89 <109626048+FernandoPR89@users.noreply.github.com>
2024-01-16 18:34:16 -0600

    Tiene un error al presionar igual y de nuevo igual

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import logoApp from './imagenes/logo.jpg'
+import Boton from './components/Boton.jsx'
+import Pantalla from './components/Pantalla.jsx';
+import BotonClear from './components/BotonClear.jsx';
+import { useState } from 'react';
+import {evaluate} from 'mathjs';
 
 function App() {
+  
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val)
+  };
+
+  const calcularResultado = () => {
+    if(input){
+      setInput(evaluate(input))
+    }
+    else {
+      alert("Por favor ingrese valores")
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-contenedor'>
+        <img src={logoApp}
+        className='logo' 
+        alt='logo' />
+      </div>
+      <div className='contenedor-calculadora '>
+        <Pantalla input={input} />
+
+        <div className='fila'>
+        <Boton manejarClic={agregarInput}>1</Boton>
+        <Boton manejarClic={agregarInput}>2</Boton>
+        <Boton manejarClic={agregarInput}>3</Boton>
+        <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+        <Boton manejarClic={agregarInput}>4</Boton>
+        <Boton manejarClic={agregarInput}>5</Boton>
+        <Boton manejarClic={agregarInput}>6</Boton>
+        <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+        <Boton manejarClic={agregarInput}>7</Boton>
+        <Boton manejarClic={agregarInput}>8</Boton>
+        <Boton manejarClic={agregarInput}>9</Boton>
+        <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+        <Boton manejarClic={calcularResultado}>=</Boton>
+        <Boton manejarClic={agregarInput}>0</Boton>
+        <Boton manejarClic={agregarInput}>.</Boton>
+        <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClic={() => setInput('')}>
+            Clear
+          </BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Pantalla.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import '../stylesheet/Pantalla.css'
+const Pantalla = ({ input }) => (
+  <diV className='input'>
+      {input}
+  </diV>
+);
+
+export default Pantalla
